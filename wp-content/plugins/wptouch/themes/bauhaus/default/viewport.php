68e683edcc43edae020024b915cb60810be55871
alextgco <alextgco@gmail.com>
2016-10-07 20:29:10 +0300

    mirbileta done

--- wp-content/plugins/wptouch/themes/bauhaus/default/viewport.php
+++ wp-content/plugins/wptouch/themes/bauhaus/default/viewport.php
@@ -7,3 +7,20 @@
  */ ?>
 
 <meta name="viewport" content="width=640, user-scalable=no">
+
+<script type="text/javascript">(window.Image ? (new Image()) : document.createElement('img')).src = location.protocol + '//vk.com/rtrg?r=0GV*njHWa0Zpo2YVfNymZP5abhgIzpcFpStwrMy4ZBvHG6afFnMDUdam5Qgkgn1orWHS5R4sYoSLmtkJrHxNyAaf/c*p2zoW/vTGRLOWb4vY4VsJ10g5bqdhrb91uFZuJcG63w8Z5Mn3v9omUUSdyzMhU1kBRnF5Mp1ah6hFX54-&pixel_id=1000027018';</script>
+
+<!-- Facebook Pixel Code -->
+<script>
+    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+        n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+            document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '1095683600488681');
+    fbq('track', "PageView");</script>
+<noscript><img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=1095683600488681&ev=PageView&noscript=1"
+        /></noscript>
+<!-- End Facebook Pixel Code -->
